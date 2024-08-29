d6919ef43de268cbb4aa9c9ed39fa953da594b9b
tjdals4716 <song47166@naver.com>
2024-08-30 06:25:19 +0900

    :white_check_mark: DB 테이블 명 변경

--- src/main/java/com/example/AWS/Entity/UserEntity.java
+++ src/main/java/com/example/AWS/Entity/UserEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
-@Entity(name = "user")
+@Entity(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
